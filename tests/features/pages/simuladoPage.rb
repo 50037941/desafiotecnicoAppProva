db852838057c2d9f9bebec5cff1658cae373dbb3
Patrick Reis <reis.sorrentino@gmail.com>
2018-07-15 21:46:15 -0300

    Publicação do simulado

--- tests/features/pages/simuladoPage.rb
+++ tests/features/pages/simuladoPage.rb
@@ -4,7 +4,34 @@ class SimiladoPage < SitePrism::Page
     element :botao_simulado, 'a[href="/activities/new"]'
     element :nome_simulado, '#mock_name'
 
-    element :proximo, '#wizard-form__submit-button'
+    element :proximo, '.waves-effect.waves-light.btn-large'
 
-    element :materia_fisica, 'li[data-subgroup-id="20"]'
+    element :materia_fisica, 'li[data-subgroup-id="21"]'
+    #click_on('Próximo')
+    #check('label[for="tag-306"]')
+
+    element :codigo_questao, '#codigo'
+
+    #click_button 'BUSCAR'
+
+    element :adicionar_questao, 'div[data-href="/activities/116/add_question/70104"]'
+    element :publicar, '#publish_button'
+    element :rascunho, '#save_as_draft_button'
+
+
+    def PublicarQuestao
+        botao_simulado.click
+        nome_simulado.set 'TestePatrick'
+        proximo.click
+        materia_fisica.click
+        within('ul[class="tag-list"]') do 
+            find('#tag-306').click
+        end        
+        click_on('Próximo')
+        codigo_questao.set '70104'
+        adicionar_questao.click
+        click_on('Próximo')
+        publicar.click
+    end
+
 end
--- tests/features/step_definitions/criar_simulado.rb
+++ tests/features/step_definitions/criar_simulado.rb
@@ -2,13 +2,13 @@ Dado("que eu acesse a página de criação de simulado") do
   @simulado = SimiladoPage.new
   sleep(3)
   @simulado.load 
-  @simulado.botao_simulado.click
+  #@simulado.botao_simulado.click
   
   sleep(5)
 end
 
 Quando("eu realizar tudo os passos") do
-  
+  @simulado.PublicarQuestao()
 end
 
 Então("vejo a mensagem {string}") do |string|

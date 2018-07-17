2c11ab782764f469bc4edbcffd3e7703e0a89189
Patrick Reis <reis.sorrentino@gmail.com>
2018-07-16 23:51:56 -0300

    Concluindo edição de perfil

--- tests/features/pages/perfil_alunoPage.rb
+++ tests/features/pages/perfil_alunoPage.rb
@@ -4,9 +4,9 @@ class PerfilAluno < SitePrism::Page
     element :dadosPerfil, 'a[href="/users/edit_enrollment_data/initial"]'
     element :estado, '#user_estado_id'
     element :cidade, '#user_cidade_id'
-    element :escola, '#select2-drop-mask'
+    element :escola, '#select2-chosen-1'
     element :buscaEscola, '#s2id_autogen1_search'
-    element :resultadoBusca, '#select2-result-label-2'
+    element :resultadoBusca, 'ul[id="select2-results-1"]'
     element :serie, '#user_enrollment_attributes_grade_id'
     element :botao_Salvar, 'input[value="Salvar dados"]'
 
@@ -14,9 +14,11 @@ class PerfilAluno < SitePrism::Page
         dadosPerfil.click
         estado.select(estadoA)
         cidade.select(cidadeA)
-        escola.select(escolaA)
-       #buscaEscola.select(escolaA)
-        resultadoBusca.click
+        escola.click
+        sleep(2)
+        buscaEscola.set escolaA
+        sleep(2)
+        buscaEscola.send_keys(:enter)
         serie.select(serieA)
         botao_Salvar.click
     end

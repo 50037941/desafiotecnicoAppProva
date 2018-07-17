dba38d4f97bd1215a73c000f43692b4214e65796
Patrick Reis <reis.sorrentino@gmail.com>
2018-07-17 12:24:51 -0300

    Correção publicação do simulado

--- tests/features/pages/simuladoPage.rb
+++ tests/features/pages/simuladoPage.rb
@@ -9,26 +9,28 @@ class SimiladoPage < SitePrism::Page
     element :materia_fisica, 'li[data-subgroup-id="21"]'
     #click_on('Próximo')
     #check('label[for="tag-306"]')
-
     element :codigo_questao, '#codigo'
+    element :selecionaEletricidade, 'label[for="tag-306"]'
 
     #click_button 'BUSCAR'
-
-    element :adicionar_questao, 'div[data-href="/activities/116/add_question/70104"]'
+    element :button_buscar, 'button[type="submit"]'
+    element :adicionar_questao, 'div[class="btn add-question"]'
     element :publicar, '#publish_button'
     element :rascunho, '#save_as_draft_button'
 
 
-    def PublicarQuestao
+    def PublicarQuestao()
         botao_simulado.click
         nome_simulado.set 'TestePatrick'
         proximo.click
         materia_fisica.click
-        within('ul[class="tag-list"]') do 
-            find('#tag-306').click
-        end        
+        sleep(2)
+        selecionaEletricidade.click
+        #find('label[for="tag-306"]').click 
+        sleep(2)  
         click_on('Próximo')
         codigo_questao.set '70104'
+        button_buscar.click
         adicionar_questao.click
         click_on('Próximo')
         publicar.click
